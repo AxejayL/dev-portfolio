cb5c789d5367c190d6d69c2b05dcb65557021a40
AxejayL <69445998+AxejayL@users.noreply.github.com>
2024-09-28 01:00:00 +0800

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; 
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,8 +7,8 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App /> {}
-  </React.StrictMode> 
+    <App />
+  </React.StrictMode>
 );
 
 
