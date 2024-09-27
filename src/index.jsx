1a057e4e87f9a2a8bea8efac66f0be55e86c7416
AxejayL <69445998+AxejayL@users.noreply.github.com>
2024-09-28 00:43:50 +0800

    Fix dependency issues

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // Use 'react-dom/client' for createRoot
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+    <App />,  // Added trailing comma here
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
